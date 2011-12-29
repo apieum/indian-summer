fa704497def8acef0b6e4e024f02bb3726faba11
Grégory Salvan <apis@apieum.com>
2011-12-29 03:08:35 +0100

    correct lines indentation on Collection.php

--- lib/automne/Config/Collection.php
+++ lib/automne/Config/Collection.php
@@ -12,10 +12,10 @@
  *
  */
 require_once 'Abstract'.DIRECTORY_SEPARATOR.'SeekableCollection.php';
- /**
- * This collection stores objects herited from type 'ATM_Config_Abstract' 
+/**
+ * This collection stores objects herited from type 'ATM_Config_Abstract'
  * and provides search on objects classes and names (return of getName).
- *  
+ *
  * @category Automne
  * @package  Config
  * @author   Gregory Salvan <gregory.salvan@apieum.com>
@@ -25,31 +25,30 @@ require_once 'Abstract'.DIRECTORY_SEPARATOR.'SeekableCollection.php';
  */
 class ATM_Config_Collection extends ATM_Config_SeekableCollection_Abstract
 {
-    public static $objectsType='ATM_Config_Abstract';
+    public static $objectsType = 'ATM_Config_Abstract';
     protected $classes    = array();
     protected $names      = array();
     protected $properties = array('objIds', 'content', 'names', 'classes');
-
-	/**
-	 * Construct a new Collection binded to $parent if set
-	 * fill the object with values of the given argument if not empty
-	 * 
-	 * @param array  $array  optional values to set
-	 * @param object $parent a parent object to modify when these values changes
-	 */
-	public function __construct ($array=array(), $parent=null) 
-	{
-	    $this->bind(&$parent);
-	    foreach ( (array) $array as $value) {
-	        $this->append($value);
-	    }
-	}
+    /**
+     * Construct a new Collection binded to $parent if set
+     * fill the object with values of the given argument if not empty
+     *
+     * @param array  $array  optional values to set
+     * @param object $parent a parent object to modify when these values changes
+     */
+    public function __construct ($array=array(), $parent=null)
+    {
+        $this->bind(&$parent);
+        foreach ( (array) $array as $value) {
+            $this->append($value);
+        }
+    }
     /**
      * Set content value at offset $offset
-     * 
+     *
      * @param integer $offset the indice to set
      * @param mixed   &$value the value of content at this offset
-     * 
+     *
      * @return mixed $value
      */
     protected function classesSetter($offset, &$value)
@@ -59,10 +58,10 @@ class ATM_Config_Collection extends ATM_Config_SeekableCollection_Abstract
     }
     /**
      * Set content value at offset $offset
-     * 
+     *
      * @param integer $offset the indice to set
      * @param mixed   &$value the value of content at this offset
-     * 
+     *
      * @return mixed $value
      */
     protected function namesSetter($offset, &$value)
@@ -72,11 +71,11 @@ class ATM_Config_Collection extends ATM_Config_SeekableCollection_Abstract
     }
 
     /**
-     * Return a new collection object binded to this, 
-     * which contains only objects of class $class 
-     * 
+     * Return a new collection object binded to this,
+     * which contains only objects of class $class
+     *
      * @param string $class a class name
-     * 
+     *
      * @return object a collection with objects of class $class
      */
     public function getCollectionOfClass($class)
@@ -84,11 +83,11 @@ class ATM_Config_Collection extends ATM_Config_SeekableCollection_Abstract
         return $this->searchAndGet($class, 'classes');
     }
     /**
-     * Return a new collection object binded to this, 
-     * which contains only objects with name $name 
-     * 
+     * Return a new collection object binded to this,
+     * which contains only objects with name $name
+     *
      * @param string $name an object name
-     * 
+     *
      * @return object a collection fill with objects named $name
      */
     public function getCollectionOfName($name)
